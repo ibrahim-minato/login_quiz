03bb5c8966f8e6ab3d012c8c27a2da742dcd1174
Minato sibri <120091487+ibrahim-minato@users.noreply.github.com>
2025-09-17 11:03:06 +0000

    Update login_quiz.py

--- login_quiz.py
+++ login_quiz.py
@@ -1,35 +1,73 @@
 #!/usr/bin/env python3
 # -- coding: utf-8
-
 '''
-this program uses flask to create a login page, registration page and saves it in infor.txt file
-if you login there is a quiz page and after answers the questions it take you to a new page to show you result
+Flask login and Quiz web application
+------------------------------------
+This project is a simple flask application where users can:
+- login  with a username,password
+-register with a username, email, password
+- it saves the details in text file
+- take a multiple-choice quiz after logging in or registing
+-view their results at the end
+
+The sytem uses password hashing for better security,
+Developed as a group project by
+[DECISIVE INNOVATORS]
 '''
 
 from flask import Flask,render_template,request,redirect,url_for
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 questions = [
-    {'question': 'What is the capital of France?', 
+    {'question': 'What is the result of 3**2 in python?', 
      'options':{
-         'A':'Berlin',
-         'B':'Madrid', 
-         'C':'Paris', 
-         'D': 'Rome'
+         'A':'6',
+         'B':'9', 
+         'C':'8', 
+         'D':'5'
          } ,
-     'answer': 'C'
-    },
-    {'question': 'which Planet is know as the red planet?',
-     'options': {
-         'A':'Earth',
-         'B':'Mars',
-         'C':'Jupiter',
-         'D':'Saturn'
-         },
      'answer': 'B'
+    },
+    {'question': 'which people developed python?',
+     'options': {
+         'A':'Guido Van Rossum',
+         'B':'Mark Zuckerberg',
+         'C':'James Gosling',
+         'D':'Charles Babbage'
+         },
+     'answer': 'A'
+     },
+    {'question': 'What is the result of "hello.upper()"?',
+     'options': {
+         'A':'HELLO',
+         'B':'Hello',
+         'C':'hello',
+         'D':'Error'
+         },
+     'answer': 'A'
+     },
+     {'question': 'which of the following is NOT a programming paradigm?',
+     'options': {
+         'A':'Object-Oriented',
+         'B':'Functional ',
+         'C':'Procedural',
+         'D':'Graphical'
+         },
+     'answer': 'D'
+     },
+         {'question': 'which of the following is a compiled language?',
+     'options': {
+         'A':'Python',
+         'B':'JavaScript',
+         'C':'C++',
+         'D':'Ruby'
+         },
+     'answer': 'C'
      }
+
      ]
 
 
@@ -48,7 +86,7 @@ def login():
                 creds = line.strip().split(',')
                 if len(creds)>=2:
                     name, pwd =creds[0], creds[1]
-                    if name == username and pwd ==password:
+                    if name == username and check_password_hash(pwd, password):
                         return redirect(url_for('successfull'))
         return 'Invalid input '  
     return 'user login not found' 
@@ -67,9 +105,10 @@ def register():
                     if len(detail) >= 2 and detail[0] == username:
                         return 'Username already exists'
 
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
         with open('infor.txt', 'a') as f:  
-            f.write(f'{username},{password},{email}\n')    
-        return 'Registration successful'   
+            f.write(f'{username},{hashed_password},{email}\n')    
+        return redirect(url_for('successfull'))
 
     
     return render_template('register.html')
